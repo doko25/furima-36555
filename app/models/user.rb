a807bc9c7b9a94a0aa9926667232af170e9e3978
doko25 <do.kouhei@outlook.com>
2021-09-01 15:33:40 +0900

    バリテーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :nickname, presence: true
+  validates :name_last, presence: true
+  validates :name_first, presence: true
+  validates :name_kana_last, presence: true
+  validates :name_kana_first, presence: true
+  validates :birth, presence: true
 end
