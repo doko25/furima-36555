b6914529cfa73b3f9855fd1fbd5a7dcd30e99162
doko25 <do.kouhei@outlook.com>
2021-09-03 21:23:46 +0900

    モデル、コントローラー、ルーティング作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_061827) do
+ActiveRecord::Schema.define(version: 2021_09_03_120931) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.text "detail"
+    t.integer "category_id"
+    t.integer "condition_id"
+    t.integer "deliver_price_id"
+    t.integer "area_id"
+    t.integer "days_id"
+    t.integer "price"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
