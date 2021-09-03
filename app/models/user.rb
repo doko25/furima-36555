5fcc3857c85fa91fac89928dd0cab197f08ccae9
doko25 <do.kouhei@outlook.com>
2021-09-03 15:30:33 +0900

    バリデーション修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
   
   with_options presence: true do
-    validates :email
-    validates :password, format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
     validates :nickname
     validates :birth
 
