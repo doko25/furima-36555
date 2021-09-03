7d6f3a61a2f0a33e6b4b506131161377e93366b8
doko25 <do.kouhei@outlook.com>
2021-09-03 21:27:55 +0900

    createアクション作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,14 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :detail, :category_id, :condition_id, :deliver_price_id, :area_id, :days_id, :price, :user)
+  end
+
 end
