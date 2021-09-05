d96999b9a52d352f7cdf9740c59da05eae038147
doko25 <do.kouhei@outlook.com>
2021-09-05 18:54:02 +0900

    モデル修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,21 +4,16 @@ class Item < ApplicationRecord
     validates :image
     validates :name
     validates :detail
-    validates :category_id
-    validates :condition_id
-    validates :deliver_price_id
-    validates :area_id
-    validates :days_id
-      validates :price, numericality: { less_than_or_equal_to: 9999999, greater_than_or_equal_to: 300 }
+    validates :price, numericality: { less_than_or_equal_to: 9999999, greater_than_or_equal_to: 300 }
+    with_options  numericality: { other_than: 1 , message: "can't be blank"} do
+      validates :category_id
+      validates :condition_id
+      validates :deliver_price_id
+      validates :area_id
+      validates :days_id
+    end
   end
-  
-  with_options  numericality: { other_than: 1 , message: "can't be blank"} do
-    validates :category_id
-    validates :condition_id
-    validates :days_id
-    validates :deliver_price_id
-    validates :area_id
-  end
+
 
 
 
@@ -28,7 +23,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
-  belongs_to :deliver_price
+  belongs_to :deliverPrice
   belongs_to :area
   belongs_to :days
   belongs_to :user
