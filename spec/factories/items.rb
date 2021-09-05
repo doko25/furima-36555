eb80b5de45d5ff0d66b1b2a80da4fe7f3f4019d2
doko25 <do.kouhei@outlook.com>
2021-09-05 20:46:48 +0900

    FactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    name              { 'test' }
+    detail            { 'testdetail' }
+    category_id       { 'メンズ' }
+    condition_id      { '新品・未使用' }
+    deliver_price_id  { '着払い(購入者負担)' }
+    area_id           { '北海道' }
+    days_id           { '4~7日で発送' }
+    price             { '500' }
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'ユーザー新規登録' do
+    context '出品できるとき' do
+      it 'image,name, detail,category_id,condition_id,deliver_price_id,area_id, days_id,priceが存在すれば出品できる' do
+      end
+
+      it 'priceが300~9999999であれば出品できる' do
+      end
+    end
+
+    context '出品できない時' do
+      it 'imageが空だと出品できない' do
+      end
+
+      it 'nameが空だと出品できない' do
+      end
+
+      it 'detailが空だと出品できない' do
+      end
+
+      it 'category_idが「--」であれば出品できない' do
+      end
+
+      it 'condition_idが「--」であれば出品できない' do
+      end
+
+      it 'deliver_price_idが「--」であれば出品できない' do
+      end
+
+      it 'area_idが「--」であれば出品できない' do
+      end
+
+      it 'days_idが「--」であれば出品できない' do
+      end
+
+      it 'priceが空だと出品できない' do
+      end
+
+      it 'priceが300以下だと出品できない' do
+      end
+
+      it 'priceが9999999以上だと出品できない' do
+      end
+    end
+  end
 end
