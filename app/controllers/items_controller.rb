554c22f1e51a90ec17b531384368012237c5ea86
doko25 <do.kouhei@outlook.com>
2021-09-07 19:45:34 +0900

    コントローラー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,16 @@ class ItemsController < ApplicationController
   end
 
 
+  def edit
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
 
   private
