6dac441e7e5d30fa643653bc4d1cb776b43045ad
doko25 <do.kouhei@outlook.com>
2021-09-16 10:07:51 +0900

    購入画面遷移設定修正

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,5 +1,5 @@
 class BuysController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!
   before_action :set_item, except: :create
 
   def index
